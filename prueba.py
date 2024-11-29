957e70968188269e970cf09d5310c6354fe95363
HengryAl <160436907+HengryAl@users.noreply.github.com>
2024-11-29 10:34:09 +0000

    defini la funcion de selecionar el topico aun por pulir

--- prueba.py
+++ prueba.py
@@ -4,23 +4,23 @@ import random
 
 def elegir_topico(selec):
     if selec == 1:
-        print('sele1')
+        salii = lista_animales
     elif selec == 2:
-        print('sele2')
+        salii = lista_cine
     elif selec == 3:
-        print('selec3')
+        salii = lista_tecnologia
     else:
-        print('no selec')
-        
+        salii = 'no selec'
+    return salii    
 lista_animales = ['perro', 'gato' ,'mamífero', 'ave' ,'reptil', 'peces', 'insectos' ,'carnívoro', 'herbívoro' ,'hábitat']
-lista_cine = ['película' 'director' 'actor' 'guion' 'escena' 'estreno' 'género' 'trama' 'reparto' 'banda sonora']
-lista_tecnologia = [ 'innovación' 'software' 'hardware' 'redes' 'internet' 'computadora' 'aplicación' 'código' 'nube' 'seguridad']
+lista_cine = ['película', 'director', 'actor', 'guion', 'escena', 'estreno', 'género' ,'trama' ,'reparto' ,'banda ,sonora']
+lista_tecnologia = [ 'innovación', 'software', 'hardware', 'redes', 'internet', 'computadora', 'aplicación', 'código' ,'nube' ,'seguridad']
 
 paso = random.randint(0,9)
 
 print(paso)
-
-
+entra = int(input('Escribe 1, 2, 3, 4'))
+salida = elegir_topico(entra)
 ver = lista_animales[paso]
 
 print(ver)
@@ -28,3 +28,5 @@ print(ver)
 numee = len(ver)
 
 print(numee)
+
+print(salida)
